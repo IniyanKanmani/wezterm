84904d14e956b8a3c71e4c46a50d87331f7a783a
IniyanKanmani <kiniyan002@gmail.com>
2024-10-08 18:42:12 +0100

    added background and blur

--- wezterm.lua
+++ wezterm.lua
@@ -26,44 +26,46 @@ config.window_close_confirmation = "NeverPrompt"
 
 config.window_decorations = "RESIZE"
 
--- config.background = {
--- 	{
--- 		source = {
--- 			Color = "#0F1114",
--- 		},
--- 		width = "100%",
--- 		height = "100%",
--- 		opacity = 1,
--- 	},
--- 	{
--- 		source = {
--- 			File = os.getenv("HOME") .. "/.config/wezterm/background/gargantua.jpg",
--- 		},
--- 		width = "100%",
--- 		height = "Contain",
--- 		repeat_x = "NoRepeat",
--- 		repeat_y = "NoRepeat",
--- 		repeat_x_size = "100%",
--- 		repeat_y_size = "100%",
--- 		vertical_align = "Middle",
--- 		horizontal_align = "Center",
--- 		opacity = 1,
--- 	},
--- 	{
--- 		source = {
--- 			Color = "#000000",
--- 		},
--- 		width = "100%",
--- 		height = "100%",
--- 		opacity = 0.6,
--- 	},
--- }
-
 config.window_padding = {
 	left = 10,
 	right = 10,
 	top = 15,
 	bottom = 0,
 }
+
+config.background = {
+	{
+		source = {
+			Color = "#0F1114",
+		},
+		width = "100%",
+		height = "100%",
+		opacity = 1,
+	},
+	{
+		source = {
+			File = os.getenv("HOME") .. "/.config/wezterm/background/gargantua.jpg",
+		},
+		width = "100%",
+		height = "Contain",
+		repeat_x = "NoRepeat",
+		repeat_y = "NoRepeat",
+		repeat_x_size = "100%",
+		repeat_y_size = "100%",
+		vertical_align = "Middle",
+		horizontal_align = "Center",
+		opacity = 1,
+	},
+	{
+		source = {
+			Color = "#000000",
+		},
+		width = "100%",
+		height = "100%",
+		opacity = 0.6,
+	},
+}
+
+config.macos_window_background_blur = 10
 
 return config

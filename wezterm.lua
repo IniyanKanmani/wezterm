9ad3ed13db487cdc26460d0f8d2f57a37314c225
IniyanKanmani <kiniyan002@gmail.com>
2024-10-09 21:06:05 +0100

    modified blur without background

--- wezterm.lua
+++ wezterm.lua
@@ -33,39 +33,40 @@ config.window_padding = {
 	bottom = 0,
 }
 
-config.background = {
-	{
-		source = {
-			Color = "#0F1114",
-		},
-		width = "100%",
-		height = "100%",
-		opacity = 1,
-	},
-	{
-		source = {
-			File = os.getenv("HOME") .. "/.config/wezterm/background/gargantua.jpg",
-		},
-		width = "100%",
-		height = "Contain",
-		repeat_x = "NoRepeat",
-		repeat_y = "NoRepeat",
-		repeat_x_size = "100%",
-		repeat_y_size = "100%",
-		vertical_align = "Middle",
-		horizontal_align = "Center",
-		opacity = 1,
-	},
-	{
-		source = {
-			Color = "#000000",
-		},
-		width = "100%",
-		height = "100%",
-		opacity = 0.6,
-	},
-}
-
+config.window_background_opacity = 0.7
 config.macos_window_background_blur = 10
+
+-- config.background = {
+-- 	{
+-- 		source = {
+-- 			Color = "#0F1114",
+-- 		},
+-- 		width = "100%",
+-- 		height = "100%",
+-- 		opacity = 1.0,
+-- 	},
+-- 	{
+-- 		source = {
+-- 			File = os.getenv("HOME") .. "/.config/wezterm/background/gargantua.jpg",
+-- 		},
+-- 		width = "100%",
+-- 		height = "Contain",
+-- 		repeat_x = "NoRepeat",
+-- 		repeat_y = "NoRepeat",
+-- 		repeat_x_size = "100%",
+-- 		repeat_y_size = "100%",
+-- 		vertical_align = "Middle",
+-- 		horizontal_align = "Center",
+-- 		opacity = 1.0,
+-- 	},
+-- 	{
+-- 		source = {
+-- 			Color = "#000000",
+-- 		},
+-- 		width = "100%",
+-- 		height = "100%",
+-- 		opacity = 0.5,
+-- 	},
+-- }
 
 return config
